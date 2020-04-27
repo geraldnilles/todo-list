8ede74a6d4a118ce1179c3ecf9f0bb358d703ac7
Gerald Nilles <geraldnilles@gmail.com>
2020-04-26 18:29:43 -0700

    Get button and check callbacks working

--- todos/static/main.js
+++ todos/static/main.js
@@ -1,3 +1,30 @@
 
 
+// Mark and Item as complete when 
+function bind_checkboxes(){
+    var checkboxes = document.querySelectorAll("input[type='checkbox']");
+	for (var i = 0; i < checkboxes.length; i++){
+		var c = checkboxes[i];
+		c.onchange = function(e){
+            var li = e.target.closest("li");
+            if (e.target.checked){
+                li.classList.add("list-group-item-dark");
+            } else {
+                li.classList.remove("list-group-item-dark");
+            }
+		}
+	}
+}
+
+function bind_form(){
+    var f = document.querySelector("form");
+    f.onsubmit = function(e){
+        alert("Submit!");
+    }
+}
+
+
+bind_form();
+bind_checkboxes();
+
 
